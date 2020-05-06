040646ca716951806c40efb2158dae6f98c28a74
Paula Garcia <paugarcia22@gmail.com>
2020-05-05 21:50:11 -0400

    beginner

---
+++ javascript-beginner.js
@@ -0,0 +1,33 @@
+'green'=='blue'
+
+/*
+var myString = 'Paunu';
+
+var ASCII_value = myString.charCodeAt(0);
+
+console.log(ASCII_value);
+*/
+
+// "Up up\n\tdown down"
+
+// console.log('The man whispered, \"please speak to me.\"')
+
+// "Hello" + 5*10
+
+// 2 + 2*2*11 - 4
+
+// 3 <= 3
+
+// 2 + 10 - 19 + 4 - 90 + 1
+
+// -20 + -19 - (-10) - (-1) + 24
+
+// (10/5) * 4 - 20
+
+// 4096 % 12
+
+// 3
+
+// 2 + 890.3
+
+// console.log('hola peps');
