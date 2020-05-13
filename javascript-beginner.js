edd4b9cb37357a6c75bca0bf80f5f143590c1958
Paula Garcia <paugarcia22@gmail.com>
2020-05-13 14:39:40 -0400

    wear

--- javascript-beginner.js
+++ javascript-beginner.js
@@ -1,4 +1,136 @@
 /*
+ * Programming Quiz: What do I Wear? (3-7)
+ *
+ * Using if/else statements, create a series of logical expressions that logs the size of a t-shirt based on the measurements of:
+ *   - shirtWidth
+ *   - shirtLength
+ *   - shirtSleeve
+ *
+ * Use the chart above to print out one of the following correct values:
+ *   - S, M, L, XL, 2XL, or 3XL
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `shirtWidth`, `shirtLength`, and `shirtSleeve`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should use logical expressions
+ * 4. Your code should produce the expected output
+ * 6. Your code should not be empty
+ * 7. BE CAREFUL ABOUT THE EXACT CHARACTERS TO BE PRINTED.
+ */
+ 
+
+
+// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+var shirtWidth = 18;
+var shirtLength = 29;
+var shirtSleeve = 8.47;
+
+/*
+ * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, expectedSize]:
+ * [18, 28, 8.13, 'S']
+ * [19.99, 28.99, 8.379, 'S']
+ * [20, 29, 8.38, 'M']
+ * [22, 30, 8.63, 'L']
+ * [24, 31, 8.88, 'XL']
+ * [26, 33, 9.63, '2XL']
+ * [27.99, 33.99, 10.129, '2XL']
+ * [28, 34, 10.13, '3XL']
+ * [18, 29, 8.47, 'NA']
+*/
+
+// WRITE YOUR CODE HERE
+
+if ((shirtWidth >= 18) && (shirtWidth < 20) && (shirtLength >= 28) && (shirtLength < 29) && (shirtSleeve >= 8.13) && (shirtSleeve < 8.38)) {
+  console.log('S');
+} 
+  else if ((shirtWidth >= 20) && (shirtWidth < 22) && (shirtLength === 29) && (shirtLength < 30) && (shirtSleeve === 8.38) && (shirtSleeve < 8.63)) {
+    console.log('M');
+  }
+  else if ((shirtWidth >= 22) && (shirtWidth < 24) && (shirtLength >= 30) && (shirtLength < 31) && (shirtSleeve >= 8.63) && (shirtSleeve < 8.88)) {
+    console.log('L');
+  }
+  else if ((shirtWidth >= 24) && (shirtWidth < 26) && (shirtLength >= 31) && (shirtLength < 33) && (shirtSleeve >= 8.88) && (shirtSleeve < 9.63)) {
+    console.log('XL');
+  }
+  else if ((shirtWidth >= 26) && (shirtWidth < 28) && (shirtLength >= 33) && (shirtLength < 34) && (shirtSleeve >= 9.63) && (shirtSleeve < 10.13)) {
+    console.log('2XL');
+  }
+  else if ((shirtWidth >= 28) && (shirtLength >= 34) && (shirtSleeve >= 10.13)) {
+    console.log('3XL');
+  }
+  else {
+    console.log('NA')
+  }
+
+
+/*
+ * Programming Quiz: Ice Cream (3-6)
+ *
+ * Write a single if statement that logs out the message:
+ * 
+ * "I'd like two scoops of __________ ice cream in a __________ with __________."
+ * 
+ * ...only if:
+ *   - flavor is "vanilla" or "chocolate"
+ *   - vessel is "cone" or "bowl"
+ *   - toppings is "sprinkles" or "peanuts"
+ *
+ * We're only testing the if statement and your boolean operators. 
+ * It's okay if the output string doesn't match exactly.
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `flavor`, `vessel`, and `toppings`
+ * 2. Your code should have an `if` statement
+ * 3. Your code should use logical expressions
+ * 4. Your code should work with 
+ *  - `flavor=vanilla`, `vessel=cone`, and `toppings=sprinkles`
+ *  - `flavor=vanilla`, `vessel=cone`, and `toppings=peanuts`
+ *  - `flavor=vanilla`, `vessel=bowl`, and `toppings=sprinkles`
+ *  - `flavor=vanilla`, `vessel=bowl`, and `toppings=peanuts`
+ * 
+ *  - `flavor=chocolate`, `vessel=cone`, and `toppings=sprinkles`
+ *  - `flavor=chocolate`, `vessel=cone`, and `toppings=peanuts`
+ *  - `flavor=chocolate`, `vessel=bowl`, and `toppings=sprinkles`
+ *  - `flavor=chocolate`, `vessel=bowl`, and `toppings=peanuts`
+ *   
+ * 5. Your code should NOT log (print) anything when 
+ *  - the flavor is something other than "vanilla" or "chocolate"
+ *  - the vessel is something other than "cone" or "bowl"
+ *  - the toppings is something other than "sprinkles" or "peanuts"
+ * 
+ * 6. Your code should not be empty
+ */
+ 
+// change the values of `flavor`, `vessel`, and `toppings` to test your code
+/*
+var flavor = "vanilla";
+var vessel = "cone";
+var toppings = "peanuts";
+*/
+// Add your code here
+
+// if (flavor === 'vanilla' || flavor === 'chocolate') {
+//   if (vessel === 'cone' || vessel === 'bowl') 
+//   if (toppings === 'sprinkles' || toppings === 'peanuts') {
+//     console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}.`)
+//   }
+// }
+
+//correct answer (because it has one conditional): 
+
+/*
+if ((flavor === 'vanilla' || flavor === 'chocolate') && (vessel === 'cone' || vessel === 'bowl') && (toppings === 'sprinkles' || toppings === 'peanuts')) {
+    console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}.`)
+  }
+*/
+
+
+
+/*
 Using the flowchart below, write the code to represent checking your balance at the ATM. The yellow diamonds represent conditional statements and the blue rectangles with rounded corners represent what should be printed to the console.
 */
 /*
@@ -14,6 +146,7 @@ Using the flowchart below, write the code to represent checking your balance at 
  * 5. BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED. 
  */
 
+/*
 var balance = 0;
 var checkBalance = true;
 var isActive = false;
@@ -50,7 +183,7 @@ if (checkBalance === true) {
 else {
   console.log("Thank you. Have a nice day!");
 }
-
+*/
 
 
 
