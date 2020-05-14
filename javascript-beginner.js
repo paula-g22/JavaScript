296e172d4f0c6d134338fcd1f97e5faa346998ba
Paula Garcia <paugarcia22@gmail.com>
2020-05-14 19:00:40 -0400

    switch

--- javascript-beginner.js
+++ javascript-beginner.js
@@ -1,4 +1,53 @@
 /*
+ * Programming Quiz: Back to School (3-9)
+ *
+ * Write a switch statement to set the average salary of a person based on their type of completed education.
+ *
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have the variables `education`, and `salary`
+ * - Your code should include a switch statement
+ * - Your code should produce the expected output
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+// change the value of `education` to test your code
+var education = "a Bachelor's degree";
+
+// set the value of this based on a person's education
+var salary = 0;
+
+// your code goes here
+
+switch (education) {
+  case 'no high school diploma':
+    salary = 25636;
+    break;
+  case 'a high school diploma':
+    salary = 35256;
+    break;
+  case "an Associate's degree":
+    salary = 41496;
+    break;
+  case "a Bachelor's degree":
+    salary = 59124;
+    break;
+  case "a Master's degree":
+    salary = 69732;
+    break;
+  case "a Professional degree":
+    salary = 89960;
+    break;
+  case "a Doctoral degree":
+    salary = 84396;
+}
+
+console.log(`In 2015, a person with ${education} earned an average of $${salary.toLocaleString("en-US")}/year.`);
+
+/*
  * Programming Quiz - Navigating the Food Chain (3-8)
  *
  * Use a series of ternary operator to set the category to one of the following:
@@ -22,9 +71,10 @@
  */
  
 // change the values of `eatsPlants` and `eatsAnimals` to test your code
+/*
 var eatsPlants = true;
 var eatsAnimals = true;
-
+*/
 /*
  * Test your code agaist the followig possible input/output combinations of (`eatsPlants`, `eatsAnimals`, expected output):
  * - (true, true, omnivore)
@@ -33,10 +83,11 @@ var eatsAnimals = true;
  * - (false, false, undefined)
  */
 
+/*
 var category = eatsPlants ? (eatsAnimals ? 'omnivore' : 'herbivore') : (eatsAnimals ? 'carnivore' : 'undefined');
 
 console.log(category);
-
+*/
 
 /*
 Imagine your software is running the inventory of a shoe store, a client needs to know in what colors do you have a particular shoe.
