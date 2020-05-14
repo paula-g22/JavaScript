4e4acd929ea20d114ab88219c9746fff74f1bce3
Paula Garcia <paugarcia22@gmail.com>
2020-05-13 23:48:41 -0400

    ternary

--- javascript-beginner.js
+++ javascript-beginner.js
@@ -1,4 +1,77 @@
 /*
+ * Programming Quiz - Navigating the Food Chain (3-8)
+ *
+ * Use a series of ternary operator to set the category to one of the following:
+ *   - "herbivore" if an animal eats plants
+ *   - "carnivore" if an animal eats animals
+ *   - "omnivore" if an animal eats plants and animals
+ *   - undefined if an animal doesn't eat plants or animals
+ *
+ * Notes
+ *   - use the variables `eatsPlants` and `eatsAnimals` in your ternary expressions
+ *   - `if` statements aren't allowed ;-)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have the variables `eatsPlants`, `eatsAnimals`
+ * - Your code should include ternary statements. Do not use if....else statement. 
+ * - Your code should produce the expected output
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+// change the values of `eatsPlants` and `eatsAnimals` to test your code
+var eatsPlants = true;
+var eatsAnimals = true;
+
+/*
+ * Test your code agaist the followig possible input/output combinations of (`eatsPlants`, `eatsAnimals`, expected output):
+ * - (true, true, omnivore)
+ * - (false, true, carnivore)
+ * - (true, false, herbivore)
+ * - (false, false, undefined)
+ */
+
+var category = eatsPlants ? (eatsAnimals ? 'omnivore' : 'herbivore') : (eatsAnimals ? 'carnivore' : 'undefined');
+
+console.log(category);
+
+
+/*
+Imagine your software is running the inventory of a shoe store, a client needs to know in what colors do you have a particular shoe.
+
+Instructions
+Complete this switch statement with 3 possible colors: Red, Green and Blue.
+
+The function needs to return true if the color is available or false if the color is not available.
+*/
+
+/*
+function getColor(selection)
+{
+	switch(selection){
+	    case 'RED':
+		  case 'Red':
+		  case 'red':
+		  case 'GREEN':
+		  case 'Green':
+		  case 'green':
+		  case 'BLUE':
+		  case 'Blue':
+		  case 'blue':
+		    return true; 
+		    //break; //peps me explicas porque hay q break aca
+	    default :
+	    	return false;//return false because the user pick a unavailable color
+	      //break;               
+	}
+}
+
+console.log(getColor('yellow'))
+*/
+
+/*
  * Programming Quiz: What do I Wear? (3-7)
  *
  * Using if/else statements, create a series of logical expressions that logs the size of a t-shirt based on the measurements of:
@@ -23,10 +96,11 @@
 
 
 // change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+/*
 var shirtWidth = 18;
 var shirtLength = 29;
 var shirtSleeve = 8.47;
-
+*/
 /*
  * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, expectedSize]:
  * [18, 28, 8.13, 'S']
@@ -41,6 +115,8 @@ var shirtSleeve = 8.47;
 */
 
 // WRITE YOUR CODE HERE
+
+/*
 
 if ((shirtWidth >= 18) && (shirtWidth < 20) && (shirtLength >= 28) && (shirtLength < 29) && (shirtSleeve >= 8.13) && (shirtSleeve < 8.38)) {
   console.log('S');
@@ -64,6 +140,7 @@ if ((shirtWidth >= 18) && (shirtWidth < 20) && (shirtLength >= 28) && (shirtLeng
     console.log('NA')
   }
 
+*/
 
 /*
  * Programming Quiz: Ice Cream (3-6)
@@ -240,11 +317,12 @@ else {
 // /* IMPLEMENTATION LOGIC*/
 
 // Initial values
+/*
 var weapon = "";
 var solved = false;
 var room = "dining room";
 var suspect = "Mr. Parkes";
-
+*/
 // /*
 // * To help solve this mystery, write a combination of conditional statements that:
 // * 1. sets the value of weapon based on the room and
