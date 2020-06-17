b9f8e0df7ac0c13e36cc7bb142acc831cf749a38
Paula Garcia <paugarcia22@gmail.com>
2020-06-17 01:12:35 -0400

    regex

--- regex.js
+++ regex.js
@@ -2,8 +2,38 @@ let arr = ['Hi my name is Hernan and I wanted to know if..', 'When she was born 
 'When the pandemic was over the name Justin kept resonating in his mind.', 
 'If they can come together and give their business the name Coco'];
 
-const regex = /
+const reg = /name(\w?)(\s\w*)?\s([A-Z]\w*)/g
 
-for (let e of arr) {
-    e.match(regex)
+
+function getNames(arr){
+    let names = []
+    let allNames = []
+    for (let e of arr) {
+        names = [...names, ...(e.match(reg))]
+    }
+    if (names.length > 1) {
+        for (let e of names) {
+            allNames.push(e.substring(e.lastIndexOf(' ')).trim())
+        } 
+    } 
+    return allNames;
 }
+
+console.log(getNames(arr));
+
+
+
+
+
+// for (let i=0; i<names.length; i++) {
+//     names[i].substring(3)
+// }
+
+// console.log(names);
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
+
+// console.log(found);
+// expected output: Array ["T", "I"]
