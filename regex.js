84cdbc69618b943ad19c4a89d0eb16a0e782d47e
Paula Garcia <paugarcia22@gmail.com>
2020-06-17 15:29:58 -0400

    regex

--- regex.js
+++ regex.js
@@ -2,24 +2,41 @@ let arr = ['Hi my name is Hernan and I wanted to know if..', 'When she was born 
 'When the pandemic was over the name Justin kept resonating in his mind.', 
 'If they can come together and give their business the name Coco'];
 
-const reg = /name(\w?)(\s\w*)?\s([A-Z]\w*)/g
+// const reg = /name(\w?)(\s\w*)?\s([A-Z]\w*)/g
+const reg = /name[ a-z]+([A-Z]\w*)/
 
+
+// function getNames(arr){
+//     let names = []
+//     let allNames = []
+//     for (let e of arr) {
+//         names = [...names, ...(e.match(reg))]
+//     }
+//     if (names.length > 1) {
+//         for (let e of names) {
+//             allNames.push(e.substring(e.lastIndexOf(' ')).trim())
+//         } 
+//     } 
+//     return allNames;
+// }
+
+// function getNames(arr){
+//     let names = []
+//     for (let e of arr) {
+//         names = [...names, ...(e.match(reg))]
+//     }
+//     return names.map(e => e.replace(reg, '$3'))
+// }
 
 function getNames(arr){
     let names = []
-    let allNames = []
     for (let e of arr) {
-        names = [...names, ...(e.match(reg))]
+        names = [...names, e.match(reg)[1]]
     }
-    if (names.length > 1) {
-        for (let e of names) {
-            allNames.push(e.substring(e.lastIndexOf(' ')).trim())
-        } 
-    } 
-    return allNames;
+    return names;
 }
 
-console.log(getNames(arr));
+console.log(getNames(arr))
 
 
 
@@ -31,9 +48,9 @@ console.log(getNames(arr));
 
 // console.log(names);
 
-const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
-const regex = /[A-Z]/g;
-const found = paragraph.match(regex);
+// const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+// const regex = /[A-Z]/g;
+// const found = paragraph.match(regex);
 
 // console.log(found);
 // expected output: Array ["T", "I"]
