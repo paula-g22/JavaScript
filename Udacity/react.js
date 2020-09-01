3a92249582c16b1ad22de57dd6c5e8708ce22fe3
Paula Garcia <paugarcia22@gmail.com>
2020-08-31 20:43:28 -0400

    react

--- Udacity/react.js
+++ Udacity/react.js
@@ -1,3 +1,36 @@
+/*
+ * Programming Quiz: Building Classes and Subclasses (2-3)
+ */
+
+class Vehicle {
+	constructor(color = 'blue', wheels = 4, horn = 'beep beep') {
+		this.color = color;
+		this.wheels = wheels;
+		this.horn = horn;
+	}
+
+	honkHorn() {
+		console.log(this.horn);
+	}
+}
+
+class Bicycle extends Vehicle {
+    constructor(color, wheels = 2, horn = 'honk honk'){
+        super(color, wheels, horn);
+    }
+}
+
+// your code goes here
+
+//tests
+const myVehicle = new Vehicle();
+myVehicle.honkHorn(); // beep beep
+const myBike = new Bicycle();
+myBike.honkHorn(); // honk honk
+
+
+
+
 function Sandwich(bread, meat, vegetables){
     this.bread = bread;
     this.meat = meat;
