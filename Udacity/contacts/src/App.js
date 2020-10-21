3c3d966a117ab081e2fe218475b2d59f85a12636
Paula Garcia <paugarcia22@gmail.com>
2020-10-21 14:15:03 -0400

    mental math

--- Udacity/contacts/src/App.js
+++ Udacity/contacts/src/App.js
@@ -5,37 +5,70 @@ import './App.css';
 
 class App extends Component {
   state = {
-    value1: this.ranNum(),
-    value2: this.ranNum(),
-    value3: this.ranNum(),
+    ...this.generateState(),
     numQuestions: 0,
     numCorrect: 0
   }
-  ranNum() {
-    return Math.floor(Math.random() * 100)
+  ranNum(max) {
+    return Math.floor(Math.random() * max)
+  }
+
+  generateState() {
+    let value1 = this.ranNum(100);
+    let value2 = this.ranNum(100);
+    let value3 = this.ranNum(100);
+    let proposedAns = this.ranNum(3) + value1 + value2 + value3;
+    return {
+      value1: value1,
+      value2: value2,
+      value3: value3,
+      proposedAnswer: proposedAns,
+    }
   }
   
-  proposedAnswer = Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3;
+  // proposedAnswer = Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3;
     
-  forOnClick = (answer, bool) => {
-    if ((answer === this.proposedAnswer && bool === 'true') || 
-    (answer !== this.proposedAnswer && bool === 'false')){
-      this.setState(() => ({
-        numCorrect: this.state.numCorrect + 1,
-        numQuestions: this.state.numQuestions + 1
-      })) 
-    }
-    else {
-      this.setState(() => ({
-        numQuestions: this.state.numQuestions + 1
-      }))
-    }
+  // forOnClick = (answer, bool) => {
+  //   if ((answer === this.proposedAnswer && bool === 'true') || 
+  //   (answer !== this.proposedAnswer && bool === 'false')){
+  //     this.setState(() => ({
+  //       numCorrect: this.state.numCorrect + 1,
+  //       numQuestions: this.state.numQuestions + 1
+  //     })) 
+  //   }
+  //   else {
+  //     this.setState(() => ({
+  //       numQuestions: this.state.numQuestions + 1
+  //     }))
+  //   }
+  // }
+
+  forOnClick = (bool) => {
+    const answer = this.state.value1 + this.state.value2 + this.state.value3
+    const newState = this.generateState();
+
+    this.setState(() => {
+      if ((answer === this.state.proposedAnswer && bool === 'true') || 
+      (answer !== this.state.proposedAnswer && bool === 'false')) {
+        return {numCorrect: this.state.numCorrect + 1}
+      } 
+      }) 
+    this.setState(() => ({...newState, numQuestions: this.state.numQuestions + 1}));
+    // this.setState(() => {
+    //   const { value1, value2, value3, proposedAnswer } = this.generateState();
+    //   return {
+    //     numQuestions: this.state.numQuestions + 1,
+    //     value1: value1,
+    //     value2: value2,
+    //     value3: value3,
+    //     proposedAnswer: proposedAnswer
+    //   } 
+    // }) 
   }
 
 
   render() {
-    const { value1, value2, value3, numQuestions, numCorrect } = this.state;
-    const ans = value1 + value2 + value3;
+    const { value1, value2, value3, proposedAnswer, numQuestions, numCorrect } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -45,14 +78,14 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${this.proposedAnswer}`}</p>
+            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
           </div>
           <button 
-            onClick={() => this.forOnClick(ans, 'true')} name='true'>
+            onClick={() => this.forOnClick('true')} name='true'>
             True
           </button>
           <button
-            onClick={() => this.forOnClick(ans, 'false')} name='false'>
+            onClick={() => this.forOnClick('false')} name='false'>
             False
             </button>
           <p className="text">
