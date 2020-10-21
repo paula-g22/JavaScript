62eeeccbe96e435b92ff7d691d7f51b22ed13b40
Paula Garcia <paugarcia22@gmail.com>
2020-10-21 16:44:12 -0400

    controlled components

--- Udacity/contacts/src/App.js
+++ Udacity/contacts/src/App.js
@@ -1,30 +1,105 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
-// import ListContacts from './ListContacts'
+import ListContacts from './ListContacts'
 
-class App extends Component {
-  state = {
-    ...this.generateState(),
-    numQuestions: 0,
-    numCorrect: 0
-  }
-  ranNum(max) {
-    return Math.floor(Math.random() * max)
-  }
-
-  generateState() {
-    let value1 = this.ranNum(100);
-    let value2 = this.ranNum(100);
-    let value3 = this.ranNum(100);
-    let proposedAns = this.ranNum(3) + value1 + value2 + value3;
-    return {
-      value1: value1,
-      value2: value2,
-      value3: value3,
-      proposedAnswer: proposedAns,
+// const contacts = [
+//     {
+//       "id": "karen",
+//       "name": "Karen Isgrigg",
+//       "handle": "@karen_isgrigg",
+//       "avatarURL": "http://localhost:5001/karen.jpg"
+//     },
+//     {
+//       "id": "richard",
+//       "name": "Richard Kalehoff",
+//       "handle": "@richardkalehoff",
+//       "avatarURL": "http://localhost:5001/richard.jpg"
+//     },
+//     {
+//       "id": "tyler",
+//       "name": "Tyler McGinnis",
+//       "handle": "@tylermcginnis",
+//       "avatarURL": "http://localhost:5001/tyler.jpg"
+//     }
+//    ];
+  
+  // class ContactList extends React.Component {
+  //   render() {
+  //     const people = this.props.contacts 
+  
+  //     return <ol>
+  //       {people.map((person) => (
+  //         <li key={person.name}>{person.name}</li>
+  //       ))}
+  //     </ol>
+  //   }
+  // }
+  
+  class App extends Component {
+    state = {
+      contacts: [
+        {
+          "id": "karen",
+          "name": "Karen Isgrigg",
+          "handle": "@karen_isgrigg",
+          "avatarURL": "http://localhost:5001/karen.jpg"
+        },
+        {
+          "id": "richard",
+          "name": "Richard Kalehoff",
+          "handle": "@richardkalehoff",
+          "avatarURL": "http://localhost:5001/richard.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "handle": "@tylermcginnis",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+       ]
+      }
+      removeContact = (contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.filter((c) => c.id !== contact.id)
+        }))
+      }
+    
+    render() {
+      return (
+        <div className="App">
+          <ListContacts 
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}/>
+        </div>
+      );
     }
   }
+  
+  export default App;
+
+// class App extends Component {
+//   state = {
+//     ...this.generateState(),
+//     numQuestions: 0,
+//     numCorrect: 0
+//   }
+//   ranNum(max) {
+//     return Math.floor(Math.random() * max)
+//   }
+
+//   generateState() {
+//     let value1 = this.ranNum(100);
+//     let value2 = this.ranNum(100);
+//     let value3 = this.ranNum(100);
+//     let proposedAns = this.ranNum(3) + value1 + value2 + value3;
+//     return {
+//       value1: value1,
+//       value2: value2,
+//       value3: value3,
+//       proposedAnswer: proposedAns,
+//     }
+//   }
   
   // proposedAnswer = Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3;
     
@@ -43,17 +118,19 @@ class App extends Component {
   //   }
   // }
 
-  forOnClick = (bool) => {
-    const answer = this.state.value1 + this.state.value2 + this.state.value3
-    const newState = this.generateState();
+  // forOnClick = (bool) => {
+  //   const answer = this.state.value1 + this.state.value2 + this.state.value3
+  //   const newState = this.generateState();
 
-    this.setState(() => {
-      if ((answer === this.state.proposedAnswer && bool === 'true') || 
-      (answer !== this.state.proposedAnswer && bool === 'false')) {
-        return {numCorrect: this.state.numCorrect + 1}
-      } 
-      }) 
-    this.setState(() => ({...newState, numQuestions: this.state.numQuestions + 1}));
+  //   this.setState(() => {
+  //     if ((answer === this.state.proposedAnswer && bool === 'true') || 
+  //     (answer !== this.state.proposedAnswer && bool === 'false')) {
+  //       return {numCorrect: this.state.numCorrect + 1}
+  //     } 
+  //     }) 
+  //   this.setState(() => ({...newState, numQuestions: this.state.numQuestions + 1}));
+
+  //old way^above is much more concise
     // this.setState(() => {
     //   const { value1, value2, value3, proposedAnswer } = this.generateState();
     //   return {
@@ -64,116 +141,43 @@ class App extends Component {
     //     proposedAnswer: proposedAnswer
     //   } 
     // }) 
-  }
-
-
-  render() {
-    const { value1, value2, value3, proposedAnswer, numQuestions, numCorrect } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="game">
-          <h2>Mental Math</h2>
-          <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
-          </div>
-          <button 
-            onClick={() => this.forOnClick('true')} name='true'>
-            True
-          </button>
-          <button
-            onClick={() => this.forOnClick('false')} name='false'>
-            False
-            </button>
-          <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
-          </p>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
-
-
-// const contacts = [
-//   {
-//     "id": "karen",
-//     "name": "Karen Isgrigg",
-//     "handle": "@karen_isgrigg",
-//     "avatarURL": "http://localhost:5001/karen.jpg"
-//   },
-//   {
-//     "id": "richard",
-//     "name": "Richard Kalehoff",
-//     "handle": "@richardkalehoff",
-//     "avatarURL": "http://localhost:5001/richard.jpg"
-//   },
-//   {
-//     "id": "tyler",
-//     "name": "Tyler McGinnis",
-//     "handle": "@tylermcginnis",
-//     "avatarURL": "http://localhost:5001/tyler.jpg"
 //   }
-//  ];
 
-// class ContactList extends React.Component {
+
 //   render() {
-//     const people = this.props.contacts 
-
-//     return <ol>
-//       {people.map((person) => (
-//         <li key={person.name}>{person.name}</li>
-//       ))}
-//     </ol>
-//   }
-// }
-
-// class App extends Component {
-//   state = {
-//     contacts: [
-//       {
-//         "id": "karen",
-//         "name": "Karen Isgrigg",
-//         "handle": "@karen_isgrigg",
-//         "avatarURL": "http://localhost:5001/karen.jpg"
-//       },
-//       {
-//         "id": "richard",
-//         "name": "Richard Kalehoff",
-//         "handle": "@richardkalehoff",
-//         "avatarURL": "http://localhost:5001/richard.jpg"
-//       },
-//       {
-//         "id": "tyler",
-//         "name": "Tyler McGinnis",
-//         "handle": "@tylermcginnis",
-//         "avatarURL": "http://localhost:5001/tyler.jpg"
-//       }
-//      ]
-//     }
-//     removeContact = (contact) => {
-//       this.setState((currentState) => ({
-//         contacts: currentState.contacts.filter((c) => c.id !== contact.id)
-//       }))
-//     }
-  
-//   render() {
+//     const { value1, value2, value3, proposedAnswer, numQuestions, numCorrect } = this.state;
 //     return (
 //       <div className="App">
-//         <ListContacts 
-//           contacts={this.state.contacts}
-//           onDeleteContact={this.removeContact}/>
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">ReactND - Coding Practice</h1>
+//         </header>
+//         <div className="game">
+//           <h2>Mental Math</h2>
+//           <div className="equation">
+//             <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+//           </div>
+//           <button 
+//             onClick={() => this.forOnClick('true')} name='true'>
+//             True
+//           </button>
+//           <button
+//             onClick={() => this.forOnClick('false')} name='false'>
+//             False
+//             </button>
+//           <p className="text">
+//             Your Score: {numCorrect}/{numQuestions}
+//           </p>
+//         </div>
 //       </div>
 //     );
 //   }
 // }
 
 // export default App;
+
+
+// 
 
 // const value1 = Math.floor(Math.random() * 100);
 // const value2 = Math.floor(Math.random() * 100);
--- Udacity/contacts/src/ListContacts.js
+++ Udacity/contacts/src/ListContacts.js
@@ -1,63 +1,88 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-function ListContacts (props) {
-    return (
-        <>
-        <h3>This is my list</h3>
-        <ol className='contact-list'>
-            {props.contacts.map((contact) => (
-                <li key={contact.id} className='contact-list-item'>
-                    <div 
-                        className='contact-avatar'
-                        style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}
-                    ></div>
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.handle}</p>
-                    </div>
-                    <button 
-                        className='contact-remove'
-                        onClick={() => props.onDeleteContact(contact)}
-                        >
-                        Remove
-                    </button>
-                </li>
-            ))}
-        </ol>
-        </>
-    )
-}
-
-
-// class ListContacts extends Component {
-//     render() {
-//         return (
-//             <>
-//                 <h3>This is my list</h3>
-//             <ol className='contact-list'>
-//                 {this.props.contacts.map((contact) => (
-//                     <li key={contact.id} className='contact-list-item'>
-//                         <div 
-//                             className='contact-avatar'
-//                             style={{
-//                                 backgroundImage: `url(${contact.avatarURL})`
-//                             }}
-//                         ></div>
-//                         <div className='contact-details'>
-//                             <p>{contact.name}</p>
-//                             <p>{contact.handle}</p>
-//                         </div>
-//                         <button className='contact-remove'>
-//                             {this.props.okay}
-//                         </button>
-//                     </li>
-//                 ))}
-//             </ol>
-//             </>
-//         )
-//     }
+// function ListContacts (props) {
+//     return (
+//         <>
+//         <h3>This is my list</h3>
+//         <ol className='contact-list'>
+//             {props.contacts.map((contact) => (
+//                 <li key={contact.id} className='contact-list-item'>
+//                     <div 
+//                         className='contact-avatar'
+//                         style={{
+//                             backgroundImage: `url(${contact.avatarURL})`
+//                         }}
+//                     ></div>
+//                     <div className='contact-details'>
+//                         <p>{contact.name}</p>
+//                         <p>{contact.handle}</p>
+//                     </div>
+//                     <button 
+//                         className='contact-remove'
+//                         onClick={() => props.onDeleteContact(contact)}
+//                         >
+//                         Remove
+//                     </button>
+//                 </li>
+//             ))}
+//         </ol>
+//         </>
+//     )
 // }
+
+// ListContacts.propTypes = {
+//     contacts: PropTypes.array.isRequired,
+//     onDeleteContact: PropTypes.func.isRequired
+// }
+
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+    state = {
+        query: ''
+    }
+    updateQuery = (query) => {
+        this.setState(()=> ({
+            query: query.trim()
+        }))
+    }
+    render() {
+        return (
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input 
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+            <ol className='contact-list'>
+                {this.props.contacts.map((contact) => (
+                    <li key={contact.id} className='contact-list-item'>
+                        <div 
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}
+                        ></div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            {this.props.okay}
+                        </button>
+                    </li>
+                ))}
+            </ol>
+            </div>
+        )
+    }
+}
 
 export default ListContacts
