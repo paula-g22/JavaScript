391db37200b689e7cfaed25fec6622e8fd063af7
Paula Garcia <paugarcia22@gmail.com>
2020-10-23 20:42:13 -0400

    adding users

--- Udacity/contacts/Exercise1.js
+++ Udacity/contacts/Exercise1.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 
-class CreateNewUser extends Component {
+class CreateUserForm extends Component {
   render(){
     return (
       <form>
         <input 
           type='text'
-          placeholder='First Name' />
+          placeholder='First Name' 
+          />
+      <button disabled={this.fieldIsEmpty} onClick={this.addUser}>Add</button>
       </form>
     )
   }
+}
 
+function DisplayUser (props){
+  return(
+    <h2>Users</h2>
+    <ul className='user-list'>
+      {props.users.map((username) => (
+        <li key={userID}>{username}</li>
+      )
+      }
+      </ul>
+  )
 }
