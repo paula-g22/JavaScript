a8bb43d109a52f2ccf1dff189ae95e6b4b96da1f
Paula Garcia <paugarcia22@gmail.com>
2020-10-28 21:49:21 -0400

    creating forms

--- Udacity/contacts/Exercise1.js
+++ Udacity/contacts/Exercise1.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class CreateUserForm extends Component {
+  state = {
+    user: {
+      firstName: '',
+      lastName: '',
+      username: ''
+    },
+    userExists: false
+  }
+
+  fieldIsEmpty = () => {
+    return this.state.user.firstName === '' || this.state.user.lastName === '' || this.state.user.username === ''
+  }
+
   render(){
     return (
       <form>
@@ -16,7 +29,7 @@ class CreateUserForm extends Component {
             type='text'
             placeholder='Enter a username'
             />
-      <button disabled={this.fieldIsEmpty} onClick={this.addUser}>Add</button>
+      <button disabled={this.fieldIsEmpty()} onClick={this.addUser}>Add</button>
       </form>
     )
   }
@@ -24,16 +37,21 @@ class CreateUserForm extends Component {
 
 function UserList (props){
   return(
+    <>
     <h2>Users</h2>
     <ul className='user-list'>
       {props.users.map((username) => 
-        <li key={userID}>{username}</li>
+      <li key={userID}>{username}</li>
+      )}
       </ul>
-  ))}
+  </>
+  )
 }
 
 function DisplayGames (props){
   return(
-
+    <div>
+      <p>This user has played {users.gamesplayed} games.</p>
+    </div>
   )
 }
