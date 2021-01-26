5060ceeae25e209d38d3f9029c7c26dfc6765f8a
Paula Garcia <paugarcia22@gmail.com>
2021-01-25 22:32:07 -0500

    Udemy challenges

--- Udemy Javascript/Coding-challenges.js
+++ Udemy Javascript/Coding-challenges.js
@@ -1,11 +1,40 @@
+'use strict';
+
+//coding-challenge#2 arrays
+
+function calcTip (bill){
+    return bill < 50 || bill > 300 ? bill*.20 : bill*.15;
+}
+
+console.log(calcTip(100))
+
+//coding-challenge#1 functions
+
+// const calcAverage = (num1, num2, num3) => (num1 + num2 + num3) / 3;
+
+// let avgKoalas = calcAverage(23, 34, 27)
+
+// let avgDolphins = calcAverage(85, 54, 41);
+
+// function checkWinner (avgKoalas, avgDolphins){
+//     if(avgKoalas >= avgDolphins*2){
+//         return `Koalas win (${avgKoalas} vs. ${avgDolphins})`
+//     } else if (avgDolphins >= avgKoalas*2){
+//         return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`
+//     } else {
+//         return `Neither team wins`
+//     }
+// }
+
+// console.log(checkWinner(avgKoalas, avgDolphins))
 
 //coding-challenge-4
 
-const bill = 430;
-let tip = bill < 50 || bill > 300 ? bill*.20 : bill*.15;
+// const bill = 430;
+// let tip = bill < 50 || bill > 300 ? bill*.20 : bill*.15;
 
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total was ${bill + tip}.`);
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total was ${bill + tip}.`);
 
 //coding-challenge-3
 // let scoreDolphins = (97 + 112 + 101) / 3;
